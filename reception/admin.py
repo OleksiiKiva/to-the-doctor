470d45b191e0e6e27ea1dc468bf99e348351095a
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-12-28 12:31:49 +0200

    feat: customize the admin page

--- reception/admin.py
+++ reception/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from reception.models import Visit
+
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = (
+        "date_time",
+        "patient",
+        "type_of_visit",
+        "doctor",
+        "treatment_direction",
+        "deleted_at",
+    )
+    list_filter = (
+        "treatment_direction",
+        "type_of_visit",
+        "deleted_at",
+    )
+    search_fields = ("doctor",)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import Specialization, Patient, Doctor
+
+
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "deleted_at",)
+    list_filter = ("deleted_at",)
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "phone_number",
+        "date_of_birth",
+        "deleted_at",
+    )
+    list_filter = ("first_name",)
+    search_fields = ("last_name",)
+
+
+@admin.register(Doctor)
+class DoctorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "recertification_with",
+        "deleted_at",
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            (
+                "Additional info",
+                {"fields": ("specializations", "recertification_with",)}
+            ),
+        )
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "specializations",
+                        "recertification_with",
+                    )
+                },
+            ),
+        )
+    )

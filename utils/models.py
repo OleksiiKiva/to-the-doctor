46ee004e98fd46f5f913fa851ba161d603f5865f
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-12-28 12:02:58 +0200

    feat: add soft-delete model and soft-delete manager

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class SoftDeleteManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+
+class SoftDeleteModel(models.Model):
+    deleted_at = models.DateTimeField(null=True, blank=True, default=None)
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False) -> None:
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.deleted_at = None
+        self.save()
+
+    def hard_delete(self):
+        super(SoftDeleteModel, self).delete()

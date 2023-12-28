4d194d18453ae4aa899ed66ce9ac591437e87ee9
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-12-28 12:15:59 +0200

    feat: add models patient doctor specialization visits

--- reception/models.py
+++ reception/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+from users.models import Patient, Specialization
+from utils.models import SoftDeleteModel
+
+VISIT_CHOICES = (
+    ("INIT", "Initial"),
+    ("REPT", "Repeat"),
+)
+
+
+class Visit(SoftDeleteModel):
+    treatment_direction = models.ForeignKey(
+        Specialization,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    date_time = models.DateTimeField(default=datetime.now)
+    doctor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="visits"
+    )
+    type_of_visit = models.CharField(
+        max_length=4,
+        choices=VISIT_CHOICES,
+        default="REPT",
+    )
+    patient = models.ForeignKey(
+        Patient,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="visits"
+    )
+
+    class Meta:
+        ordering = ("date_time",)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+from utils.models import SoftDeleteModel
+
+
+class Specialization(SoftDeleteModel):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Doctor(AbstractUser, SoftDeleteModel):
+    recertification_with = models.DateField(default=date.today)
+    specializations = models.ManyToManyField(
+        Specialization,
+        related_name="doctors"
+    )
+
+    class Meta:
+        ordering = ("last_name",)
+        verbose_name = "doctor"
+        verbose_name_plural = "doctors"
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+    def get_absolute_url(self):
+        return reverse("users:doctor-detail", kwargs={"pk": self.pk})
+
+
+class Patient(SoftDeleteModel):
+    phone_number = models.CharField(max_length=10, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(default=date.today)
+
+    class Meta:
+        ordering = ("last_name",)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+    def get_absolute_url(self):
+        return reverse("users:patient-detail", kwargs={"pk": self.pk})
